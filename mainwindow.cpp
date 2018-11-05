535ebde48e311c57dcf452105cb33f281eaa0c0c
boss <z1412247644@hotmail.com>
2018-11-05 22:23:24 +0800

    beta0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -88,7 +88,7 @@ void MainWindow::CB_Check(){
     AddDict(dict,C_ROM,ui->CB_ROM->isChecked());
     AddDict(dict,C_VGA,ui->CB_VGA->isChecked());
     AddDict(dict,C_SDRAM,ui->CB_SDRAM->isChecked());
-    AddDict(dict,C_arduino,ui->CB_arduion->isChecked());
+    AddDict(dict,C_arduino,ui->CB_PS2->isChecked());
 }
 
 void MainWindow::on_PB_generate_clicked()
@@ -104,20 +104,24 @@ void MainWindow::on_PB_generate_clicked()
     S_qdf += "\n"
             "QUARTUS_VERSION = \"17.1.0\"\n"
             "\n"
-            "#Revisions\n"
+            "# Revisions\n"
             "\n"
-            "PROJIECT_REVISON =\"TEST\"\n"
+            "PROJECT_REVISON =\"";
+    S_qdf += ui->lineEdit_Pro_name->text();
+    S_qdf+= "\"\n"
             "\n";
     //creatFile(path_qdf,S_qdf);
 
     /*creat project.qsf*/
-    QString S_qsf,S_CLOCK,S_LED,S_SEG8,S_KEY,S_SW,S_BEEP,S_VGA;
+    QString S_qsf,S_CLOCK,S_LED,S_SEG8,S_KEY,S_SW,S_BEEP,S_VGA,S_DRAM,S_PS2;
     S_qsf = "#================================================================================\n"
             "# SYSTEM CONFIG\n"
             "#================================================================================\n"
             "set_global_assignment -name FAMILY \"Cyclone 10 LP\"\n"
             "set_global_assignment -name DEVICE 10CL006YU256C8G\n"
-            "set_global_assignment -name TOP_LEVEL_ENTITY TEST\n"     //!!need to change
+            "set_global_assignment -name TOP_LEVEL_ENTITY \"";
+    S_qsf += ui->lineEdit_Pro_name->text();
+    S_qsf+= "\"\n"
             "set_global_assignment -name ORIGINAL_QUARTUS_VERSION 18.1.0\n"
             "set_global_assignment -name LAST_QUARTUS_VERSION \"17.1.0 Standard Edition\"\n"
             "set_global_assignment -name DEVICE_FILTER_PACKAGE UFBGA\n"
@@ -133,32 +137,213 @@ void MainWindow::on_PB_generate_clicked()
     S_CLOCK = "#================================================================================\n"
               "# CLOCK\n"
               "#================================================================================\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to CLK_50\n"
+              "set_location_assignment PIN_E15 -to CLK_50\n"
               "\n";
     S_LED = "#================================================================================\n"
             "# LED\n"
             "#================================================================================\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to LED[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to LED[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to LED[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to LED[3]\n"
+            "set_location_assignment PIN_T10 -to LED[0]\n"
+            "set_location_assignment PIN_R9 -to LED[1]\n"
+            "set_location_assignment PIN_T9 -to LED[2]\n"
+            "set_location_assignment PIN_K8 -to LED[3]\n"
             "\n";
     S_SEG8 =  "#================================================================================\n"
               "# SEG8\n"
               "#================================================================================\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[0]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[1]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[2]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[3]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[4]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[5]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[6]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to SEL[7]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[0]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[1]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[2]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[3]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[4]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[5]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[6]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DIG[7]\n"
+              "set_location_assignment PIN_D16 -to SEL[0]\n"
+              "set_location_assignment PIN_C16 -to SEL[1]\n"
+              "set_location_assignment PIN_B16 -to SEL[2]\n"
+              "set_location_assignment PIN_B14 -to SEL[3]\n"
+              "set_location_assignment PIN_B13 -to SEL[4]\n"
+              "set_location_assignment PIN_B12 -to SEL[5]\n"
+              "set_location_assignment PIN_B11 -to SEL[6]\n"
+              "set_location_assignment PIN_B10 -to SEL[7]\n"
+              "set_location_assignment PIN_D15 -to DIG[0]\n"
+              "set_location_assignment PIN_C15 -to DIG[1]\n"
+              "set_location_assignment PIN_A15 -to DIG[2]\n"
+              "set_location_assignment PIN_A14 -to DIG[3]\n"
+              "set_location_assignment PIN_A13 -to DIG[4]\n"
+              "set_location_assignment PIN_A12 -to DIG[5]\n"
+              "set_location_assignment PIN_A11 -to DIG[6]\n"
+              "set_location_assignment PIN_A10 -to DIG[7]\n"
               "\n";
     S_KEY =   "#================================================================================\n"
               "# KEY\n"
               "#================================================================================\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3 V Schmitt Trigger\" -to KEY[0]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3 V Schmitt Trigger\" -to KEY[1]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3 V Schmitt Trigger\" -to KEY[2]\n"
+              "set_instance_assignment -name IO_STANDARD \"3.3 V Schmitt Trigger\" -to KEY[3]\n"
+              "set_location_assignment PIN_M1 -to KEY[0]\n"
+              "set_location_assignment PIN_F3 -to KEY[1]\n"
+              "set_location_assignment PIN_E1 -to KEY[2]\n"
+              "set_location_assignment PIN_E2 -to KEY[3]\n"
               "\n";
     S_SW = "#================================================================================\n"
            "# SW\n"
            "#================================================================================\n"
+           "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to SW[0]\n"
+           "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to SW[1]\n"
+           "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to SW[2]\n"
+           "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to SW[3]\n"
+           "set_location_assignment PIN_E16 -to SW[0]\n"
+           "set_location_assignment PIN_M16 -to SW[1]\n"
+           "set_location_assignment PIN_M15 -to SW[2]\n"
+           "set_location_assignment PIN_M2 -to SW[3]\n"
            "\n";
     S_BEEP = "#================================================================================\n"
-             "# BEEP\n"
+             "# BEEP\n"     //!SET BUZ/SW TO BUZ
              "#================================================================================\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to BEEP\n"
+            "set_location_assignment PIN_K10 -to BEEP\n"
              "\n";
+    S_PS2 = "#================================================================================\n"
+            "# PS/2\n"
+            "#================================================================================\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to PS2_DAT\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to PS2_DAT2\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to PS2_CLK\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3 V LVTTL\" -to PS2_CLK2\n"
+            "set_location_assignment PIN_A8 -to PS2_DAT\n"
+            "set_location_assignment PIN_R9 -to PS2_DAT2\n"
+            "set_location_assignment PIN_B8 -to PS2_CLK\n"
+            "set_location_assignment PIN_B9 -to PS2_CLK2\n"
+            "\n";
     S_VGA = "#================================================================================\n"
             "# VGA\n"
             "#================================================================================\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_B[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_B[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_B[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_B[3]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_G[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_G[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_G[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_G[3]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_R[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_R[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_R[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_R[3]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_HS\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to VGA_VS\n"
+            "set_location_assignment PIN_N16 -to VGA_B[0]\n"
+            "set_location_assignment PIN_L15 -to VGA_B[1]\n"
+            "set_location_assignment PIN_L16 -to VGA_B[2]\n"
+            "set_location_assignment PIN_K15 -to VGA_B[3]\n"
+            "set_location_assignment PIN_K16 -to VGA_G[0]\n"
+            "set_location_assignment PIN_J15 -to VGA_G[1]\n"
+            "set_location_assignment PIN_J16 -to VGA_G[2]\n"
+            "set_location_assignment PIN_J11 -to VGA_G[3]\n"
+            "set_location_assignment PIN_G16 -to VGA_R[0]\n"
+            "set_location_assignment PIN_G15 -to VGA_R[1]\n"
+            "set_location_assignment PIN_F16 -to VGA_R[2]\n"
+            "set_location_assignment PIN_F15 -to VGA_R[3]\n"
+            "set_location_assignment PIN_P16 -to VGA_HS\n"
+            "set_location_assignment PIN_N15 -to VGA_VS\n"
             "\n";
-
+    S_DRAM = "#================================================================================\n"
+             "# DRAM\n"
+             "#================================================================================\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[3]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[4]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[5]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[6]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[7]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[8]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[9]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[10]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[11]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_ADDR[12]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_BA[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_BA[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_CAS_N\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_CKE\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_CLK\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_CS_N\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[0]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[1]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[2]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[3]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[4]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[5]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[6]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[7]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[8]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[9]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[10]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[11]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[12]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[13]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[14]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_DQ[15]\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_LDQM\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_RAS_N\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_UDQM\n"
+            "set_instance_assignment -name IO_STANDARD \"3.3-V LVTTL\" -to DRAM_WE_N\n"
+            "set_location_assignment PIN_J2 -to DRAM_ADDR[0]\n"
+            "set_location_assignment PIN_K1 -to DRAM_ADDR[1]\n"
+            "set_location_assignment PIN_K2 -to DRAM_ADDR[2]\n"
+            "set_location_assignment PIN_L1 -to DRAM_ADDR[3]\n"
+            "set_location_assignment PIN_T8 -to DRAM_ADDR[4]\n"
+            "set_location_assignment PIN_R8 -to DRAM_ADDR[5]\n"
+            "set_location_assignment PIN_T7 -to DRAM_ADDR[6]\n"
+            "set_location_assignment PIN_R7 -to DRAM_ADDR[7]\n"
+            "set_location_assignment PIN_T6 -to DRAM_ADDR[8]\n"
+            "set_location_assignment PIN_R6 -to DRAM_ADDR[9]\n"
+            "set_location_assignment PIN_J1 -to DRAM_ADDR[10]\n"
+            "set_location_assignment PIN_T5 -to DRAM_ADDR[11]\n"
+            "set_location_assignment PIN_R5 -to DRAM_ADDR[12]\n"
+            "set_location_assignment PIN_G1 -to DRAM_BA[0]\n"
+            "set_location_assignment PIN_J6 -to DRAM_BA[1]\n"
+            "set_location_assignment PIN_C2 -to DRAM_CAS_N\n"
+            "set_location_assignment PIN_T4 -to DRAM_CKE\n"
+            "set_location_assignment PIN_R4 -to DRAM_CLK\n"
+            "set_location_assignment PIN_G2 -to DRAM_CS_N\n"
+            "set_location_assignment PIN_B5 -to DRAM_DQ[0]\n"
+            "set_location_assignment PIN_A5 -to DRAM_DQ[1]\n"
+            "set_location_assignment PIN_B6 -to DRAM_DQ[2]\n"
+            "set_location_assignment PIN_A6 -to DRAM_DQ[3]\n"
+            "set_location_assignment PIN_A4 -to DRAM_DQ[4]\n"
+            "set_location_assignment PIN_B4 -to DRAM_DQ[5]\n"
+            "set_location_assignment PIN_A3 -to DRAM_DQ[6]\n"
+            "set_location_assignment PIN_B3 -to DRAM_DQ[7]\n"
+            "set_location_assignment PIN_R3 -to DRAM_DQ[8]\n"
+            "set_location_assignment PIN_T2 -to DRAM_DQ[9]\n"
+            "set_location_assignment PIN_L2 -to DRAM_DQ[10]\n"
+            "set_location_assignment PIN_N1 -to DRAM_DQ[11]\n"
+            "set_location_assignment PIN_N2 -to DRAM_DQ[12]\n"
+            "set_location_assignment PIN_P1 -to DRAM_DQ[13]\n"
+            "set_location_assignment PIN_P2 -to DRAM_DQ[14]\n"
+            "set_location_assignment PIN_R1 -to DRAM_DQ[15]\n"
+            "set_location_assignment PIN_A2 -to DRAM_LDQM\n"
+            "set_location_assignment PIN_D1 -to DRAM_RAS_N\n"
+            "set_location_assignment PIN_T3 -to DRAM_UDQM\n"
+            "set_location_assignment PIN_B1 -to DRAM_WE_N\n"
+             "\n";
     /*if(ui->CB_clock->isChecked())
         S_qsf += S_CLOCK;
     if(ui->CB_segment->isChecked())
@@ -176,9 +361,9 @@ void MainWindow::on_PB_generate_clicked()
     //creatFile(path_qsf,S_qsf);
 
     /*creat project.v*/
-    QString S_V,S_V_CLOCK,S_V_LED,S_V_SEG8,S_V_KEY,S_V_SW,S_V_BEEP,S_V_VGA;
+    QString S_V,S_V_CLOCK,S_V_LED,S_V_SEG8,S_V_KEY,S_V_SW,S_V_BEEP,S_V_VGA,S_V_DRAM,S_V_PS2;
     S_V = "//=======================================================\n"
-          "//  This code is generated by Terasic System Builder\n"
+          "//  This code is generated by System Builder\n"
           "//=======================================================\n"
           "\n";
     S_V += "module " + ui->lineEdit_Pro_name->text() + "(\n\n";
@@ -195,7 +380,11 @@ void MainWindow::on_PB_generate_clicked()
                 "\n";
     S_V_BEEP = "//////////// BEEP //////////\n"
                 "\n";
+    S_V_PS2 = "//////////// PS/2 //////////\n"
+                "\n";
     S_V_VGA = "//////////// VGA //////////\n"
+                "\n";
+    S_V_DRAM = "//////////// DRAM //////////\n"
                 "\n";
 
     /*Choice scan*/
@@ -223,7 +412,14 @@ void MainWindow::on_PB_generate_clicked()
         S_qsf += S_VGA;
         S_V += S_V_VGA;
     }
-
+    if(ui->CB_SDRAM->isChecked()){
+        S_qsf += S_DRAM;
+        S_V += S_V_DRAM;
+    }
+    if(ui->CB_PS2->isChecked()){
+        S_qsf += S_PS2;
+        S_V += S_V_PS2;
+    }
     S_V += ");"
            "\n"
            "\n"
